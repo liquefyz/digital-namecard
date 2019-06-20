5a388930522043478c63c63d697266417698e10c
DOn Lee <don.esc@gmail.com>
2019-06-20 17:21:14 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
+
+
 document.getElementById("share-button").addEventListener("click", toggleQRCode);
-if (document.getElementById("modal-button")){
-	document.getElementById("modal-button").addEventListener("click", hideModal);
-}
 
 function toggleQRCode() {
 	if (document.getElementById("profile-pic").style.display === "none") {
@@ -11,20 +10,4 @@ function toggleQRCode() {
 		document.getElementById("profile-pic").style.display = "none";
 		document.getElementById("qr-code").style.display = "block";
 	}
-}
-
-window.onload = function() {
-	if (document.referrer === "https://firstnamebasis.app/"){
-		if (window.navigator.userAgent.toLowerCase()=== "iphone" || window.navigator.userAgent.toLowerCase() === "ipad") {
-			if (window.navigator.standalone === false) {
-				document.getElementById("opaque-background").style.display = "block";
-				document.getElementById("modal").style.display = "block";
-			}
-		}
-	}
-};
-
-function hideModal() {
-	document.getElementById("opaque-background").style.display = "none";
-	document.getElementById("modal").style.display = "none";
 }
